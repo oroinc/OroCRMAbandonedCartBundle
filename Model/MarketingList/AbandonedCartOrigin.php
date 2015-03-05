c583add61ff8d65a21337d0cc8125df98e0d2aa0
Ruslan Voitenko <psw1986@gmail.com>
2015-03-06 00:00:54 +0200

    CRM-2752: Grid of segment items for AbandonedCartList entity  - AbandonedCart Source  - AbandonedCart grid

---
+++ Model/MarketingList/AbandonedCartOrigin.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace OroCRM\Bundle\AbandonedCartBundle\Model\MarketingList;
+
+use OroCRM\Bundle\MarketingListBundle\Model\MarketingListSourceInterface;
+
+class AbandonedCartSource implements MarketingListSourceInterface
+{
+    const SOURCE_CODE = 'abandoned_cart';
+
+    /**
+     * @inheritdoc
+     */
+    public function getCode()
+    {
+        return self::SOURCE_CODE;
+    }
+}
