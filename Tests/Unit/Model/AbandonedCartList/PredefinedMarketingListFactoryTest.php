e82cba02b7dc8d570e701f5600297af296280bd3
Ruslan Voitenko <psw1986@gmail.com>
2015-03-06 12:26:56 +0200

    CRM-2681: Save process - fixes typo - adds setting of source for Abandoned Cart Marketing List

--- Tests/Unit/Model/AbandonedCartList/PredefinedMarketingListFactoryTest.php
+++ Tests/Unit/Model/AbandonedCartList/PredefinedMarketingListFactoryTest.php
@@ -5,6 +5,7 @@ namespace OroCRM\Bundle\AbandonedCartBundle\Tests\Unit\Model\AbandonedCartList;
 use Doctrine\Common\Persistence\ObjectManager;
 use OroCRM\Bundle\AbandonedCartBundle\Model\AbandonedCartList\PredefinedMarketingListFactory;
 use OroCRM\Bundle\MarketingListBundle\Entity\MarketingListType;
+use OroCRM\Bundle\MarketingListBundle\Model\MarketingListSourceInterface;
 
 class PredefinedMarketingListFactoryTest extends \PHPUnit_Framework_TestCase
 {
@@ -18,13 +19,20 @@ class PredefinedMarketingListFactoryTest extends \PHPUnit_Framework_TestCase
      */
     private $objectManager;
 
+    /**
+     * @var MarketingListSourceInterface
+     */
+    private $source;
+
     protected function setUp()
     {
         $this->objectManager = $this->getMockBuilder('Doctrine\Common\Persistence\ObjectManager')->getMock();
-        $this->factory = new PredefinedMarketingListFactory($this->objectManager);
+        $this->source = $this->getMockBuilder('OroCRM\Bundle\MarketingListBundle\Model\MarketingListSourceInterface')
+            ->getMock();
+        $this->factory = new PredefinedMarketingListFactory($this->objectManager, $this->source);
     }
 
-    public function testBuild()
+    public function testCreate()
     {
         $marketingListType = new MarketingListType(MarketingListType::TYPE_DYNAMIC);
 
@@ -32,9 +40,12 @@ class PredefinedMarketingListFactoryTest extends \PHPUnit_Framework_TestCase
             ->with('OroCRMMarketingListBundle:MarketingListType', MarketingListType::TYPE_DYNAMIC)
             ->will($this->returnValue($marketingListType));
 
+        $this->source->expects($this->once())->method('getCode')->will($this->returnValue('source_code'));
+
         $marketingList = $this->factory->create();
 
         $this->assertInstanceOf('OroCRM\Bundle\MarketingListBundle\Entity\MarketingList', $marketingList);
         $this->assertEquals($marketingListType, $marketingList->getType());
+        $this->assertEquals('source_code', $marketingList->getSource());
     }
 }
