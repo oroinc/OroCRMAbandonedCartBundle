6c65da3d14b730cf8f9c7e36c0ab5d47b8b1bcb0
alex gotgelf <gotgelf@gmail.com>
2015-04-21 01:33:36 +0200

    CRM-2624: Abandoned Shopping Cart Notifications  - review code fixes

--- Tests/Unit/Model/AbandonedCartList/CampaignFactoryTest.php
+++ Tests/Unit/Model/AbandonedCartList/CampaignFactoryTest.php
@@ -20,16 +20,13 @@ class CampaignFactoryTest extends \PHPUnit_Framework_TestCase
     public function testCreate()
     {
         $marketingList = new MarketingList();
-        $marketingList->setName('name  fg');
+        $marketingList->setName('test campaign na$me');
 
         $campaign = $this->factory->create($marketingList);
 
         $this->assertInstanceOf('OroCRM\Bundle\CampaignBundle\Entity\Campaign', $campaign);
 
-        $code = preg_replace("/[^a-z0-9]/i", "_", $marketingList->getName());
-        $code = substr($code, 0, 20) . $marketingList->getId();
-
-        $this->assertEquals($campaign->getCode(), $code);
+        $this->assertEquals($campaign->getCode(), 'test_campaign_na_me');
         $this->assertEquals($campaign->getName(), $marketingList->getName());
     }
 }
