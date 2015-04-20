bc8ea3a37aa0aafcc9edf7d25544e4fdc8bee926
alex gotgelf <gotgelf@gmail.com>
2015-04-20 14:22:24 +0200

    CRM-2624: Abandoned Shopping Cart Notifications  - review code fixes

--- Tests/Unit/EventListener/Datagrid/AbandonedCartListenerTest.php
+++ Tests/Unit/EventListener/Datagrid/AbandonedCartListenerTest.php
@@ -3,6 +3,7 @@
 namespace OroCRM\Bundle\AbandonedCartBundle\Tests\Unit\EventListener\Datagrid;
 
 use Oro\Bundle\DataGridBundle\Event\BuildBefore;
+use Oro\Bundle\DataGridBundle\Datagrid\Common\DatagridConfiguration;
 use OroCRM\Bundle\AbandonedCartBundle\EventListener\Datagrid\AbandonedCartListener;
 use OroCRM\Bundle\MarketingListBundle\Model\MarketingListHelper;
 use OroCRM\Bundle\AbandonedCartBundle\Model\AbandonedCartCampaignProviderInterface;
@@ -25,7 +26,7 @@ class AbandonedCartListenerTest extends \PHPUnit_Framework_TestCase
     protected $buildBefore;
 
     /**
-     * @var \PHPUnit_Framework_MockObject_MockObject
+     * @var \PHPUnit_Framework_MockObject_MockObject|DatagridConfiguration
      */
     protected $datagridConfig;
 
@@ -33,6 +34,11 @@ class AbandonedCartListenerTest extends \PHPUnit_Framework_TestCase
      * @var \PHPUnit_Framework_MockObject_MockObject|AbandonedCartCampaignProviderInterface
      */
     protected $abandonedCartCampaignProvider;
+
+    /**
+     * @var array
+     */
+    private $config;
 
     protected function setUp()
     {
@@ -48,6 +54,12 @@ class AbandonedCartListenerTest extends \PHPUnit_Framework_TestCase
         $this->abandonedCartCampaignProvider = $this
             ->getMock('OroCRM\Bundle\AbandonedCartBundle\Model\AbandonedCartCampaignProviderInterface');
 
+        $this->datagridConfig = $this->getMockBuilder('Oro\Bundle\DataGridBundle\Datagrid\Common\DatagridConfiguration')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->config = array('actions' => array('subscribe' => 1));
+
         $this->listener = new AbandonedCartListener(
             $this->marketingListHelper,
             $this->abandonedCartCampaignProvider
@@ -56,10 +68,6 @@ class AbandonedCartListenerTest extends \PHPUnit_Framework_TestCase
 
     public function testOnBuildBeforeWithEmptySubscribeAction()
     {
-        $this->datagridConfig = $this->getMockBuilder('Oro\Bundle\DataGridBundle\Datagrid\Common\DatagridConfiguration')
-            ->disableOriginalConstructor()
-            ->getMock();
-
         $this->buildBefore->expects($this->once())
             ->method('getConfig')
             ->will($this->returnValue(array()));
@@ -70,15 +78,13 @@ class AbandonedCartListenerTest extends \PHPUnit_Framework_TestCase
         $this->listener->onBuildBefore($this->buildBefore);
     }
 
-    public function testOnBuildBeforeWithSubscribeAction()
+    public function testOnBuildBeforeWithSubscribeActionWhenAbandonedCampaign()
     {
-        $this->datagridConfig = $this->getMockBuilder('Oro\Bundle\DataGridBundle\Datagrid\Common\DatagridConfiguration')
-            ->disableOriginalConstructor()
-            ->getMock();
+        $this->initDatagridConfig();
 
         $this->buildBefore->expects($this->once())
             ->method('getConfig')
-            ->will($this->returnValue(array('actions' => array('subscribe' => 1))));
+            ->will($this->returnValue($this->datagridConfig));
 
         $datagrid = $this->getMock('Oro\Bundle\DataGridBundle\Datagrid\DatagridInterface');
 
@@ -110,11 +116,88 @@ class AbandonedCartListenerTest extends \PHPUnit_Framework_TestCase
             ->with($this->equalTo($marketingList->getId()))
             ->will($this->returnValue($marketingList));
 
-        $marketingList
-            ->expects($this->any())
-            ->method('getId')
-            ->will($this->returnValue(1));
+        $this->abandonedCartCampaignProvider
+            ->expects($this->once())
+            ->method('getAbandonedCartCampaign')
+            ->with($marketingList)
+            ->will($this->returnValue(true));
+
+        $this->datagridConfig->expects($this->once())
+            ->method('offsetUnsetByPath')
+            ->with('[actions][subscribe]');
 
         $this->listener->onBuildBefore($this->buildBefore);
+    }
+
+    public function testOnBuildBeforeWithSubscribeActionWhenNotAbandonedCampaign()
+    {
+        $this->initDatagridConfig();
+
+        $this->buildBefore->expects($this->once())
+            ->method('getConfig')
+            ->will($this->returnValue($this->datagridConfig));
+
+        $datagrid = $this->getMock('Oro\Bundle\DataGridBundle\Datagrid\DatagridInterface');
+
+        $this->buildBefore->expects($this->once())
+            ->method('getDatagrid')
+            ->will($this->returnValue($datagrid));
+
+        $gridName = 'gridName';
+
+        $datagrid
+            ->expects($this->once())
+            ->method('getName')
+            ->will($this->returnValue($gridName));
+
+        $marketingList = $this
+            ->getMockBuilder('OroCRM\Bundle\MarketingListBundle\Entity\MarketingList')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->marketingListHelper
+            ->expects($this->once())
+            ->method('getMarketingListIdByGridName')
+            ->with($this->equalTo($gridName))
+            ->will($this->returnValue($marketingList->getId()));
+
+        $this->marketingListHelper
+            ->expects($this->once())
+            ->method('getMarketingList')
+            ->with($this->equalTo($marketingList->getId()))
+            ->will($this->returnValue($marketingList));
+
+        $this->abandonedCartCampaignProvider
+            ->expects($this->once())
+            ->method('getAbandonedCartCampaign')
+            ->with($marketingList)
+            ->will($this->returnValue(false));
+
+        $this->datagridConfig->expects($this->never())
+            ->method('offsetUnsetByPath');
+
+        $this->listener->onBuildBefore($this->buildBefore);
+    }
+
+    protected function initDatagridConfig()
+    {
+        $rawConfig = $this->config;
+        $offsetResult = $this->config['actions'];
+
+        $this->datagridConfig->expects($this->once())
+            ->method('offsetExists')
+            ->with('actions')
+            ->will(
+                $this->returnCallback(
+                    function ($offset) use ($rawConfig) {
+                        return isset($rawConfig[$offset]);
+                    }
+                )
+            );
+
+        $this->datagridConfig->expects($this->any())
+            ->method('offsetGet')
+            ->with('actions')
+            ->will($this->returnValue($offsetResult));
     }
 }
