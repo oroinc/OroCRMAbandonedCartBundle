cddb2c6368d12cd8439f69b955da7220d03e31e1
alex gotgelf <gotgelf@gmail.com>
2015-04-22 11:50:36 +0200

    CRM-2677: Conversions for Abandoned Shopping Cart Notification

--- Placeholder/CampaignsPlaceholderFilter.php
+++ Placeholder/CampaignsPlaceholderFilter.php
@@ -2,22 +2,51 @@
 
 namespace OroCRM\Bundle\AbandonedCartBundle\Placeholder;
 
+use Doctrine\Common\Persistence\ManagerRegistry;
+
+use OroCRM\Bundle\MailChimpBundle\Entity\Campaign;
+use OroCRM\Bundle\MailChimpBundle\Entity\StaticSegment;
 use OroCRM\Bundle\MarketingListBundle\Entity\MarketingList;
 use OroCRM\Bundle\AbandonedCartBundle\Model\AbandonedCartCampaignProviderInterface;
 
 class CampaignsPlaceholderFilter
 {
     /**
+     * @var ManagerRegistry
+     */
+    protected $managerRegistry;
+
+    /**
      * @var AbandonedCartCampaignProviderInterface
      */
     protected $abandonedCartCampaignProvider;
 
     /**
-     * @param AbandonedCartCampaignProviderInterface $abandonedCartCampaignProvider
+     * @var string
      */
-    public function __construct(AbandonedCartCampaignProviderInterface $abandonedCartCampaignProvider)
-    {
+    protected $staticSegmentClassName;
+
+    /**
+     * @var string
+     */
+    protected $mailchimpCampaignClassName;
+
+    /**
+     * @param ManagerRegistry $managerRegistry
+     * @param AbandonedCartCampaignProviderInterface $abandonedCartCampaignProvider
+     * @param string $staticSegmentClassName
+     * @param string $mailchimpCampaignClassName
+     */
+    public function __construct(
+        ManagerRegistry $managerRegistry,
+        AbandonedCartCampaignProviderInterface $abandonedCartCampaignProvider,
+        $staticSegmentClassName,
+        $mailchimpCampaignClassName
+    ) {
+        $this->managerRegistry = $managerRegistry;
         $this->abandonedCartCampaignProvider = $abandonedCartCampaignProvider;
+        $this->staticSegmentClassName = $staticSegmentClassName;
+        $this->mailchimpCampaignClassName = $mailchimpCampaignClassName;
     }
 
     /**
@@ -26,10 +55,39 @@ class CampaignsPlaceholderFilter
      */
     public function isApplicable($entity)
     {
-        if ($this->abandonedCartCampaignProvider->getAbandonedCartCampaign($entity)) {
+        $staticSegment = $this->getStaticSegment($entity);
+        $mailchimpCampaign = $this->getMailchimpCampaign($staticSegment);
+
+        if ($this->abandonedCartCampaignProvider->getAbandonedCartCampaign($entity) && $mailchimpCampaign) {
             return true;
-        } else {
-            return false;
         }
+
+        return false;
+    }
+
+    /**
+     * @param MarketingList $marketingList
+     * @return StaticSegment|null
+     */
+    protected function getStaticSegment(MarketingList $marketingList)
+    {
+        $staticSegment = $this->managerRegistry
+            ->getRepository($this->staticSegmentClassName)
+            ->findOneBy(['marketingList' => $marketingList]);
+
+        return $staticSegment;
+    }
+
+    /**
+     * @param StaticSegment $staticSegment
+     * @return Campaign|null
+     */
+    protected function getMailchimpCampaign(StaticSegment $staticSegment)
+    {
+        $mailchimpCampaign = $this->managerRegistry
+            ->getRepository($this->mailchimpCampaignClassName)
+            ->findOneBy(['staticSegment' => $staticSegment]);
+
+        return $mailchimpCampaign;
     }
 }
--- Tests/Unit/Placeholder/CampaignsPlaceholderFilterTest.php
+++ Tests/Unit/Placeholder/CampaignsPlaceholderFilterTest.php
@@ -2,12 +2,20 @@
 
 namespace OroCRM\Bundle\AbandonedCartBundle\Tests\Unit\Placeholder;
 
+use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\ORM\EntityRepository;
+
 use OroCRM\Bundle\AbandonedCartBundle\Placeholder\CampaignsPlaceholderFilter;
 use OroCRM\Bundle\AbandonedCartBundle\Model\AbandonedCartCampaignProviderInterface;
 use OroCRM\Bundle\MarketingListBundle\Entity\MarketingList;
+use OroCRM\Bundle\MailChimpBundle\Entity\StaticSegment;
+use OroCRM\Bundle\MailChimpBundle\Entity\Campaign;
 
 class CampaignsPlaceholderFilterTest extends \PHPUnit_Framework_TestCase
 {
+    const STAT_SEGMENT_CLASS_NAME = 'staticSegmentClassName';
+    const MAILCHIMP_CAMPAIGN_CLASS_NAME = 'mailchimpCampaignClassName';
+
     /**
      * @var \PHPUnit_Framework_MockObject_MockObject|CampaignsPlaceholderFilter
      */
@@ -18,16 +26,97 @@ class CampaignsPlaceholderFilterTest extends \PHPUnit_Framework_TestCase
      */
     protected $abandonedCartCampaignProvider;
 
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject|ManagerRegistry
+     */
+    protected $managerRegistry;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject|EntityRepository
+     */
+    protected $repository;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject|MarketingList
+     */
+    protected $marketingList;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject|StaticSegment
+     */
+    protected $staticSegment;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject|Campaign
+     */
+    protected $mailchimpCampaign;
+
     public function setUp()
     {
+        $this->managerRegistry = $this->getMockBuilder('Doctrine\Common\Persistence\ManagerRegistry')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->repository = $this->getMockBuilder('Doctrine\ORM\EntityRepository')
+            ->disableOriginalConstructor()
+            ->getMock();
+
         $this->abandonedCartCampaignProvider = $this
             ->getMock('OroCRM\Bundle\AbandonedCartBundle\Model\AbandonedCartCampaignProviderInterface');
 
-        $this->placeholderFilter = new CampaignsPlaceholderFilter($this->abandonedCartCampaignProvider);
+        $this->marketingList = $this->getMockBuilder('OroCRM\Bundle\MarketingListBundle\Entity\MarketingList')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->staticSegment = $this->getMockBuilder('OroCRM\Bundle\MailChimpBundle\Entity\StaticSegment')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->mailchimpCampaign = $this->getMockBuilder('OroCRM\Bundle\MailChimpBundle\Entity\Campaign')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->placeholderFilter = new CampaignsPlaceholderFilter(
+            $this->managerRegistry,
+            $this->abandonedCartCampaignProvider,
+            self::STAT_SEGMENT_CLASS_NAME,
+            self::MAILCHIMP_CAMPAIGN_CLASS_NAME
+        );
     }
 
-    public function testIsApplicableWhenAbandonedCart()
+    protected function tearDown()
     {
+        unset($this->managerRegistry);
+        unset($this->repository);
+        unset($this->abandonedCartCampaignProvider);
+        unset($this->marketingList);
+        unset($this->staticSegment);
+        unset($this->mailchimpCampaign);
+        unset($this->placeholderFilter);
+    }
+
+    public function testIsApplicableWhenAbandonedCartAndCampaign()
+    {
+        $this->managerRegistry
+            ->expects($this->at(0))->method('getRepository')
+            ->with(self::STAT_SEGMENT_CLASS_NAME)
+            ->will($this->returnValue($this->repository));
+
+        $this->repository
+            ->expects($this->at(0))->method('findOneBy')
+            ->with(['marketingList' => $this->marketingList])
+            ->will($this->returnValue($this->staticSegment));
+
+        $this->managerRegistry
+            ->expects($this->at(1))->method('getRepository')
+            ->with(self::MAILCHIMP_CAMPAIGN_CLASS_NAME)
+            ->will($this->returnValue($this->repository));
+
+        $this->repository
+            ->expects($this->at(1))->method('findOneBy')
+            ->with(['staticSegment' => $this->staticSegment])
+            ->will($this->returnValue($this->mailchimpCampaign));
+
         $this->abandonedCartCampaignProvider
             ->expects($this->once())
             ->method('getAbandonedCartCampaign')
@@ -35,12 +124,65 @@ class CampaignsPlaceholderFilterTest extends \PHPUnit_Framework_TestCase
 
         $this->assertEquals(
             true,
-            $this->placeholderFilter->isApplicable(new MarketingList())
+            $this->placeholderFilter->isApplicable($this->marketingList)
         );
     }
 
-    public function testIsApplicableWhenNotAbandonedCart()
+    public function testIsApplicableWhenAbandonedCartAndNoCampaign()
     {
+        $this->managerRegistry
+            ->expects($this->at(0))->method('getRepository')
+            ->with(self::STAT_SEGMENT_CLASS_NAME)
+            ->will($this->returnValue($this->repository));
+
+        $this->repository
+            ->expects($this->at(0))->method('findOneBy')
+            ->with(['marketingList' => $this->marketingList])
+            ->will($this->returnValue($this->staticSegment));
+
+        $this->managerRegistry
+            ->expects($this->at(1))->method('getRepository')
+            ->with(self::MAILCHIMP_CAMPAIGN_CLASS_NAME)
+            ->will($this->returnValue($this->repository));
+
+        $this->repository
+            ->expects($this->at(1))->method('findOneBy')
+            ->with(['staticSegment' => $this->staticSegment])
+            ->will($this->returnValue(null));
+
+        $this->abandonedCartCampaignProvider
+            ->expects($this->once())
+            ->method('getAbandonedCartCampaign')
+            ->will($this->returnValue(true));
+
+        $this->assertEquals(
+            false,
+            $this->placeholderFilter->isApplicable($this->marketingList)
+        );
+    }
+
+    public function testIsApplicableWhenMarketingList()
+    {
+        $this->managerRegistry
+            ->expects($this->at(0))->method('getRepository')
+            ->with(self::STAT_SEGMENT_CLASS_NAME)
+            ->will($this->returnValue($this->repository));
+
+        $this->repository
+            ->expects($this->at(0))->method('findOneBy')
+            ->with(['marketingList' => $this->marketingList])
+            ->will($this->returnValue($this->staticSegment));
+
+        $this->managerRegistry
+            ->expects($this->at(1))->method('getRepository')
+            ->with(self::MAILCHIMP_CAMPAIGN_CLASS_NAME)
+            ->will($this->returnValue($this->repository));
+
+        $this->repository
+            ->expects($this->at(1))->method('findOneBy')
+            ->with(['staticSegment' => $this->staticSegment])
+            ->will($this->returnValue($this->mailchimpCampaign));
+
         $this->abandonedCartCampaignProvider
             ->expects($this->once())
             ->method('getAbandonedCartCampaign')
@@ -48,12 +190,7 @@ class CampaignsPlaceholderFilterTest extends \PHPUnit_Framework_TestCase
 
         $this->assertEquals(
             false,
-            $this->placeholderFilter->isApplicable(new MarketingList())
+            $this->placeholderFilter->isApplicable($this->marketingList)
         );
-    }
-
-    protected function tearDown()
-    {
-        unset($this->placeholderFilter);
     }
 }
